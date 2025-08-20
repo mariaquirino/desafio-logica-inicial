6d9f75fdb70e5775d80df16d0297a6beb31db4e7
mariaquirino <maria.quirinof19@outlook.com>
2025-08-20 14:55:53 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("ola")
+let nomeHeroi = "Maria"
+let nivelXp = 1001
+
+if(nivelXp <= 1000){
+console.log("O Herói  "+ nomeHeroi + "  é de Nivel Ferro")
+}
+else if (nivelXp >= 1001 && nivelXp <= 2000){
+console.log("O Herói  "+ nomeHeroi + "  é de Nivel Bronze")
+}
+else if (nivelXp >= 2001 && nivelXp <= 3000){
+console.log("O Herói  "+ nomeHeroi + "  é de Nivel Prata")
+}
+else if (nivelXp >= 3001 && nivelXp <= 4000){
+console.logconsole.log("O Herói  "+ nomeHeroi + "  é de Nivel Ouro")
+}
+else if (nivelXp >= 4001 && nivelXp <= 5000){
+console.logconsole.log("O Herói  "+ nomeHeroi + "  é de Nivel Platina")
+}
+else{
+console.logconsole.log("O Herói  "+ nomeHeroi + "  é de Nivel Ascendente")
+}
+
